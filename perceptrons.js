aab951954557ed85d0d3e800d8359a4bfdc61219
Tristan Vidal <trsgot@gmail.com>
2024-03-28 21:34:46 -0500

    Add files via upload

---
+++ perceptrons.js
@@ -0,0 +1,20 @@
+// SOLVING XOR PROBLEM WITH 2 PERCEPTRONS
+
+const VALUES = [
+  [1, 1],
+  [1, 0],
+  [0, 1],
+  [0, 0]
+]
+
+const WEIGHTS = [
+  [1, 0, 0],
+  [1, 0, 0]
+]
+
+const DESIRED_OUTPUT = [
+  [0, 1, 1, 0],
+  [1, 0, 0, 1]
+]
+
+const LEARNING_RATE = 0.5
